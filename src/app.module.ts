47e39d23c184e41035fca5277ebbf74d37509c66
MrKrrot <rafita.olguin@hotmail.com>
2022-09-04 13:43:28 -0500

    Agrado PORT a variables de entorno y se utiliza la variable MONGO_URI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { StudentsModule } from './students/students.module'
+import 'dotenv/config'
 
 @Module({
     imports: [
-        MongooseModule.forRoot('mongodb://localhost/platform'),
+        MongooseModule.forRoot(process.env.MONGO_URI),
         StudentsModule
     ],
     controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import 'dotenv/config'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
@@ -17,6 +18,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config)
 
     SwaggerModule.setup('api', app, document)
-    await app.listen(5000)
+    await app.listen(process.env.PORT || 5000)
 }
 bootstrap()
